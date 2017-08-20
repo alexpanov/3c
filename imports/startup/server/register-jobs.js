3766ad20fc3a4a464638361a4f92240eb748644e
Alex Panov <thealexpanov@gmail.com>
2017-08-20 12:47:22 -0400

    Place server code correctly

--- imports/startup/server/register-jobs.js
+++ imports/startup/server/register-jobs.js
@@ -1,5 +1,5 @@
 import {SyncedCron} from 'meteor/percolate:synced-cron';
-import {updateMostStaleCityWeather} from './weather';
+import {updateMostStaleCityWeather} from '/server/weather';
 
 SyncedCron.add({
   name: 'Update the oldest weather point',
